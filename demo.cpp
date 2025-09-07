4faa60eee119d2c2ba4d571533c1774091c2199b
Harshit Gupta <harshitg31897@outlook.com>
2025-09-08 00:45:01 +0530

    first commit

---
+++ demo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+    int sum = a + b;
+    std::cout << "Sum: " << sum << std::endl;
+    return 0;
+}
