15dcabc0c98aa7e120cc3545df67a28c82b91d94
Harshit Gupta <harshitg31897@outlook.com>
2026-01-30 00:35:05 +0530

    data types saved

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int a, b;
-    std::cout << "Enter two numbers: ";
-    std::cin >> a >> b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
     int sum = a + b;
-    std::cout << "Sum: " << sum << std::endl;
+    cout << "Sum: " << sum << std::endl;
     return 0;
 }
