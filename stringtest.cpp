3ef4dcb2b83887d578db93ff6ba89d9520099cc7
Harshit Gupta <harshitg31897@outlook.com>
2026-01-30 00:32:17 +0530

    datatypes

---
+++ stringtest.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std;
+int main(){
+    string str1;
+    getline(cin, str1);
+    cout<<"The entered string is: "<<str1<<endl;
+
+    char ch, ch1='a';
+    cin>>ch;
+    cout<<"The entered character is: "<<ch<<endl;
+    return 0;
+
+}
